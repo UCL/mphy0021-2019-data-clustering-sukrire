9a6ed2f0f2fc1a06c6b8aaf27b479d75843557d5
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-02-26 15:22:57 +0000

    added basic functionality and roadmap

---
+++ clustering_numpy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def cluster():
+    parser = ArgumentParser(description='clustering_numpy.py')
+    parser.add_argument('samplesfile', type=str,
+                        help='Please give file path to samples.csv')
+    parser.add_argument('--iters', default=10, type=int,
+                        help='Please give number of iterations')
+    arguments = parser.parse_args()
+    
+   #implement a way to opent the file and read the data points
+   
+   #implement the fucntion
+
+   
+
+if __name__ == "__main__":
+    cluster()
