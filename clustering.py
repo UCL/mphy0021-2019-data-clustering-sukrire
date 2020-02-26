c834e51c34a52305764a519db9394a507b2e8480
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-02-26 14:16:20 +0000

    changed ps to point matrix: point_m to make it easier to understand

--- clustering.py
+++ clustering.py
@@ -14,33 +14,34 @@ def cluster():
 
 
     datapoints = open(arguments.samplesfile, 'r').readlines()
-    ps=[]
+    #point matrix = point_m
+    point_m=[]
     
-    for datapoint in datapoints: ps.append(tuple(map(float, datapoint.strip().split(','))))
+    for datapoint in datapoints: point_m.append(tuple(map(float, datapoint.strip().split(','))))
 
-    m=[ps[randrange(len(ps))], ps[randrange(len(ps))], ps[randrange(len(ps))]]
+    m=[point_m[randrange(len(point_m))], point_m[randrange(len(point_m))], point_m[randrange(len(point_m))]]
 
-    alloc=[None]*len(ps)
+    alloc=[None]*len(point_m)
     n=0
     
-    #plt.scatter([i[0] for i in ps],[i[1] for i in ps])
+    #plt.scatter([i[0] for i in point_m],[i[1] for i in point_m])
 
     while n<arguments.iters:
-      for i in range(len(ps)):
-        p=ps[i]
+      for i in range(len(point_m)):
+        p=point_m[i]
         d=[None] * 3
         d[0]=sqrt((p[0]-m[0][0])**2 + (p[1]-m[0][1])**2)
         d[1]=sqrt((p[0]-m[1][0])**2 + (p[1]-m[1][1])**2)
         d[2]=sqrt((p[0]-m[2][0])**2 + (p[1]-m[2][1])**2)
         alloc[i]=d.index(min(d))
       for i in range(3):
-        alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
+        alloc_ps=[p for j, p in enumerate(point_m) if alloc[j] == i]
         new_mean=(sum([a[0] for a in alloc_ps]) / len(alloc_ps), sum([a[1] for a in alloc_ps]) / len(alloc_ps))
         m[i]=new_mean
       n += 1 
 
     for i in range(3):
-      alloc_ps=[p for j, p in enumerate(ps) if alloc[j] == i]
+      alloc_ps=[p for j, p in enumerate(point_m) if alloc[j] == i]
       print("Cluster " + str(i) + " is centred at " + str(m[i]) + " and has " + str(len(alloc_ps)) + " points.")
    
 
