e691d6b274f01761626db8c78bf0d4aab7f2725c
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-02-27 15:34:56 +0000

    added initial run through of clustering for numpy implementation

--- clustering_numpy.py
+++ clustering_numpy.py
@@ -4,29 +4,43 @@ from argparse import ArgumentParser
 
 def cluster():
     parser = ArgumentParser(description='clustering_numpy.py')
-    parser.add_argument('samplesfile', type=str, help='Please give file path to samples.csv')
-    parser.add_argument('--iters', default=10, type=int,help='Please give number of iterations')
+    parser.add_argument('samplesfile', type=str, 
+                        help='Please give file path to samples.csv')
+    parser.add_argument('--iters', default=10, type=int,
+                        help='Please give number of iterations')
     arguments = parser.parse_args()
 
-    #implement a way to opent the file and read the data points
-
-    #implement the fucntion
-
-    datapoints = np.genfromtxt(arguments.samplesfile, delimiter=",", dtype='unicode')
-
-    
+    datapoints = np.genfromtxt(arguments.samplesfile, 
+                               delimiter=",", dtype='float')
     
     point1 = np.random.randint(len(datapoints))
     point2 = np.random.randint(len(datapoints))
     point3 = np.random.randint(len(datapoints))
 
-    startingpoints = [datapoints[point1,0],datapoints[point1,1],
-                      datapoints[point2,0],datapoints[point2,1],
-                      datapoints[point3,0],datapoints[point3,1],]
+    #random starting points
+    rsp = np.array([[datapoints[point1,0],datapoints[point1,1]],
+                   [datapoints[point2,0],datapoints[point2,1]],
+                   [datapoints[point3,0],datapoints[point3,1]]])
 
+    print(rsp)
     #storing array
-    str_ary = np.empty(datapoints.shape)
+    str_ary = np.empty(len(datapoints))
+    
  
+    for i in range(len(datapoints)):
+        p=datapoints
+        d=np.empty(shape=(3,1))
+        d[0]=np.abs(np.sum(p[i]-rsp[0]))
+        d[1]=np.abs(np.sum(p[i]-rsp[1]))
+        d[2]=np.abs(np.sum(p[i]-rsp[2]))
+        str_ary[i]=(np.argmin(d,axis=0))
 
+    rsp[0]=np.sum(datapoints[np.argwhere(str_ary==0)],axis=0)/len(np.argwhere(str_ary==0))
+    rsp[1]=np.sum(datapoints[np.argwhere(str_ary==1)],axis=0)/len(np.argwhere(str_ary==1))
+    rsp[2]=np.sum(datapoints[np.argwhere(str_ary==2)],axis=0)/len(np.argwhere(str_ary==2))
+
+    
+    print(rsp)
+    
 if __name__ == "__main__":
     cluster()
