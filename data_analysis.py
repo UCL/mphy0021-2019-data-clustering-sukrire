5fab24d19db77b06b3199515deba007689f67650
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-03-01 14:49:59 +0000

    merged the files into a appropriate new file

--- data_analysis.py
+++ data_analysis.py
@@ -5,6 +5,20 @@ with open('cities.json') as json_file:
 
 with open('libraries.json') as json_file:
     data2 = json.load(json_file)
+    
+def combine_func(file1,file2):
+    combine_list=[]
+    for i in range(len(file1)):
+        dict1 = {**file1[i]}
+        
+    for i in range(len(file2)):
+        dict2 = {**file2[i]}
+        
+        combine_dict = {**dict1,**dict2}
+        combine_list.append(combine_dict)
+        
+    return combine_list
+        
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump([data1,data2], f, ensure_ascii=False, indent=4)
+with open('combined.json', 'w', encoding='utf-8') as f:
+    json.dump(combine_func(data1,data2), f, ensure_ascii=False, indent=4)
