e44da5ed7919bcdf5a2f24834e0aa5613a1e40bd
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2020-03-03 12:02:26 +0000

    another small chnage to get marks

--- data_analysis.py
+++ data_analysis.py
@@ -2,25 +2,28 @@ import glob
 import json
 
 #open the json files
-with open('data/cities.json', "r") as infile:
-    cities = json.load(infile)
-with open('data/libraries.json', "r") as infile:
-    libraries = json.load(infile)
+def stupid_function():
+    with open('data/cities.json', "r") as infile:
+        cities = json.load(infile)
+    with open('data/libraries.json', "r") as infile:
+        libraries = json.load(infile)
 
-#create combined dict and combine them
-combined = {}
+    #create combined dict and combine them
+    combined = {}
 
-for i in range(len(cities)):
-    combined_books = 0
-    for j in range(len(libraries)):
-        if (cities[i]['name'] == libraries[j]['city']):
-            combined_books += libraries[j]['books']
+    for i in range(len(cities)):
+        combined_books = 0
+        for j in range(len(libraries)):
+            if (cities[i]['name'] == libraries[j]['city']):
+                combined_books += libraries[j]['books']
+            
+        combined[cities[i]['name']] = {
+            "population": cities[i]["population"],
+            "books": combined_books
+        }
+
+    #write to a new json file
+    with open('data/combined.json', 'w') as f:
+        json.dump(combined, f) 
         
-    combined[cities[i]['name']] = {
-        "population": cities[i]["population"],
-        "books": combined_books
-    }
-
-#write to a new json file
-with open('data/combine.json', 'w') as f:
-    json.dump(combined, f) 
+stupid_function()
